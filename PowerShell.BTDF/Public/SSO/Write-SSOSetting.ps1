4d64a5af648e3fdb3a21413f342700d8dca5f755
Cowen <20096232@leeds.gov.uk>
2018-05-03 17:32:23 +0100

    Added comments for Write-SSOSetting

--- PowerShell.BTDF/Public/SSO/Write-SSOSetting.ps1
+++ PowerShell.BTDF/Public/SSO/Write-SSOSetting.ps1
@@ -1,3 +1,29 @@
+#Requires -RunAsAdministrator
+
+<#
+.SYNOPSIS
+    Write settings to BTDF SSO for deployed BizTalk applications
+
+.DESCRIPTION
+    Makes use of BTDF SSOSettingsFileReader helper assembly to read all SSO settings for a deployed application
+
+.PARAMETER Application
+    Name of the BTDF deployed application to read SSO settings
+
+.PARAMETER Setting
+    Name of the setting to write to. Will create the setting if it doesn't already exist
+
+.PARAMETER Value
+    Name of the SSO setting to get the value of
+
+.EXAMPLE
+    PS C:\> Write-SSOSetting -Application Scratchpad -Setting StringSetting -Value StringValue
+    Returns a string typed value from SSO for the Scatchpad appliction for the setting "StringSetting"
+
+.OUTPUTS
+    string
+#>
+
 function Write-SSOSetting {
     [CmdletBinding()]
     Param (
