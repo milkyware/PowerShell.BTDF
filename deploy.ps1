bac0d44f8f68dad93882937f804386018405cdd1
Cameron Cowen <cameronc94@googlemail.com>
2019-02-01 01:04:22 +0000

    Improvements to backup deployment script

--- deploy.ps1
+++ deploy.ps1
@@ -1,39 +1,43 @@
 [CmdletBinding(SupportsShouldProcess=$true)]
 Param (
+    [Parameter(Mandatory=$true)]
+    [ValidateScript({$_.Exists -and ($_.Extension -eq ".psd1")})]
+    [System.IO.FileInfo]$Manifest,
     [Parameter(Mandatory = $true)]
     [string]$ApiKey,
     [Parameter()]
-    [string]$Prerelease
+    [version]$Version = (Get-Date).ToString("yy.MM.dd"),
+    [Parameter()]
+    [string]$Prerelease = ""
 )
 Process {
-    Install-PackageProvider -Name "Nuget" -Scope CurrentUser -MinimumVersion "2.8.5.201" -Force 
-    Install-Module -Name Nuget, PackageManagement, PowerShellGet -Scope CurrentUser -Force
+    Write-Debug "Version = $Version"
+    Write-Debug "Prerelease = $Prerelease"
+
+    Install-PackageProvider -Name "Nuget" -Scope CurrentUser -MinimumVersion "2.8.5.201" -Force | Out-Null
+    Install-Module -Name Nuget, PackageManagement, PowerShellGet -Scope CurrentUser -Force | Out-Null
 
     Get-Module | Remove-Module -Force -ErrorAction SilentlyContinue
 
     Write-Verbose "Installing PowerShell modules"
-    Import-Module -Name Nuget -MinimumVersion "1.3.3" -Force
-    Import-Module -Name PackageManagement -MinimumVersion "1.2.4" -Force
-    Import-Module -Name PowerShellGet -MinimumVersion "2.0.3" -Force
+    Import-Module -Name Nuget -MinimumVersion "1.3.3" -Force | Out-Null
+    Import-Module -Name PackageManagement -MinimumVersion "1.2.4" -Force | Out-Null
+    Import-Module -Name PowerShellGet -MinimumVersion "2.0.3" -Force | Out-Null
     Write-Verbose "PowerShell modules installed"
 
-    # Get-Module | Group-Object -Property Name | Where-Object {$_.Count -gt 1} | Foreach-Object {$_ | Select-Object -ExpandProperty Group | Sort-Object -Property Version -Descending | Select-Object -Skip 1} | Remove-Module -Force
+    Write-Verbose "Checking for duplicate versions of modules"
+    Get-Module | Group-Object -Property Name | Where-Object {$_.Count -gt 1} | Foreach-Object {
+        Write-Verbose "Found multiple versions of $($_.Name)"
+        $_ | Select-Object -ExpandProperty Group | Sort-Object -Property Version -Descending | Select-Object -Skip 1
+    } | Remove-Module -Force
 
     Get-Module | Select-Object ModuleType, Version, Name, Path
 
-    $manifests = Get-ChildItem -Path "$PSScriptRoot\..\" -Recurse  *.psd1
-    Write-Debug -Message "Manifests found: $($manifests.Count)"
+    Write-Verbose "Updating manifest"
+    Update-ModuleManifest -Path $Manifest.FullName -ModuleVersion $Version -Prerelease $Prerelease
+    Write-Verbose "Updated manifest"
 
-    foreach ($m in $manifests) {
-        Write-Debug "Manifest = $($m.Fullname)"
-        Write-Verbose "Module found at $($m.FullName)"
-
-        Write-Verbose "Updating manifest"
-        Update-ModuleManifest -Path $m.FullName -Prerelease $Prerelease
-        Write-Verbose "Updated manifest"
-
-        Write-Verbose "Publishing module"
-        Publish-Module -Path $m.Directory.FullName -NuGetApiKey $ApiKey -Force
-        Write-Verbose "Published module"
-    }
+    Write-Verbose "Publishing module"
+    Publish-Module -Path $Manifest.Directory.FullName -NuGetApiKey $ApiKey -Force
+    Write-Verbose "Published module"
 }
